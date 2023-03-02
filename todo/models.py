d9d9404d4ead3a0d4a0e26480fa185f3a8c2f565
ripon <ripon1995dc@gmail.com>
2023-03-02 18:01:37 +0600

    Dummy data insertion
    What?
    - created model class for todo

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+
+class ToDo(models.Model):
+    id = models.IntegerField()
+    title = models.CharField(max_length=20)
+    description = models.CharField(max_length=100)
+    status = models.CharField(max_length=20, default='New task')
