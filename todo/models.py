c2d53b671efbec3fe4ddf2f0f89f9fd5d953a0af
ripon <ripon1995dc@gmail.com>
2023-03-06 17:44:24 +0600

    Dummy data insertion
    What?
    - dummy data generation using loaddata

--- todo/models.py
+++ todo/models.py
@@ -2,15 +2,8 @@ from django.db import models
 
 
 # Create your models here.
-
-class StatusChoice(models.TextChoices):
-    choiceNew = ('NEW', 'New Task')
-    choiceRun = ('RUNNING', 'Running Task')
-    choiceFuture = ('FUTURE', 'Future Task')
-
-
 class ToDo(models.Model):
     id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=20)
     description = models.CharField(max_length=100)
-    status = models.CharField(max_length=20, default=StatusChoice.choiceNew, choices=StatusChoice.choices)
+    status = models.CharField(max_length=20, default='New Task')
