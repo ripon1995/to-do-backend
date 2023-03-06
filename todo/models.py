6915e25cea009aeb6902bd46ca19a447995b3fb7
ripon <ripon1995dc@gmail.com>
2023-03-06 13:10:50 +0600

    Dummy data insertion
    What?
    - model refactored.

--- todo/models.py
+++ todo/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 
 class ToDo(models.Model):
-    id = models.IntegerField()
+    id = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=20)
     description = models.CharField(max_length=100)
     status = models.CharField(max_length=20, default='New task')
