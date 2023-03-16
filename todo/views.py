882b7c8ca1fe589e5e2871189ac2c6d6b00ded1c
ripon <ripon1995dc@gmail.com>
2023-03-16 16:07:50 +0600

    view file updated
    what?
    - custom response created
    - returned custom response

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,7 @@
 from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
 
+from todo.custom_response import custom_response
 from todo.models import ToDo
 from todo.serializers import ToDoSerializer
 from django.http import JsonResponse
@@ -12,13 +13,13 @@ def to_do_list(request):
     if request.method == 'GET':
         to_do_list_data = ToDo.objects.all()
         serializer = ToDoSerializer(to_do_list_data, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return JsonResponse(custom_response(serializer.data), safe=False, )
 
     if request.method == 'POST':
         data = JSONParser().parse(request)
         serializer = ToDoSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
+            return JsonResponse(custom_response(serializer.data), status=201)
 
         return JsonResponse(serializer.errors, status=400)
