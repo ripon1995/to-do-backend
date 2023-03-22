b6ce08740a29487f57ddfaa3e6dd896dc4394e20
ripon <ripon1995dc@gmail.com>
2023-03-22 17:49:27 +0600

    User and ToDo model relationship
    what?
    - User retrieve view implemented

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from user.views import UserCreateView
+from user.views import UserCreateView, UserRetrieveView
 
 urlpatterns = [
     path('user/', UserCreateView.as_view()),
+    path('user/<int:pk>/', UserRetrieveView.as_view()),
 ]
