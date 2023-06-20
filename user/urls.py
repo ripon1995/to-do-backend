5c1300609b5e9c49b6fbde1b65132d1780b93140
ripon <ripon1995dc@gmail.com>
2023-06-20 15:47:50 +0600

    profile-update-api
    what?
     - me api created

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from user.views import UserCreateView, UserRetrieveView, UserPasswordUpdateView
+from user.views import UserCreateView, UserRetrieveView, UserPasswordUpdateView, Me
 
 urlpatterns = [
     path('user/', UserCreateView.as_view()),
     path('user/<int:pk>/', UserRetrieveView.as_view()),
     path('user/<int:pk>/password/', UserPasswordUpdateView.as_view()),
+    path('user/me/', Me.as_view()),
 ]
--- user/views.py
+++ user/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from rest_framework.response import Response
 from user.models import User
 from user.serializers import UserSerializer
@@ -49,3 +49,11 @@ class UserPasswordUpdateView(generics.UpdateAPIView):
         user.password = serializer.validated_data.get("password")
         serializer.save()
         return Response(serializer.data)
+
+
+class Me(generics.RetrieveAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
