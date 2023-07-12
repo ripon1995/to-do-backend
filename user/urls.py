4f5bf032c344b8571552593fdb71703d832b1a0c
ripon <ripon1995dc@gmail.com>
2023-07-12 17:42:03 +0600

    push-notification to multiple user what?  - view and url added

--- user/urls.py
+++ user/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from user.views import UserCreateView, UserRetrieveUpdateDeleteView, UserPasswordUpdateView, Me, \
-    SendPushNotificationToUser
+    SendPushNotificationToUser, SendPushNotificationToMultipleUser
 
 urlpatterns = [
     path('user/', UserCreateView.as_view()),
     path('user/<int:pk>/', UserRetrieveUpdateDeleteView.as_view()),
     path('user/<int:pk>/password/', UserPasswordUpdateView.as_view()),
     path('user/me/', Me.as_view()),
-    path('notification/<int:user_id>/', SendPushNotificationToUser.as_view())
+    path('notification/<int:user_id>/', SendPushNotificationToUser.as_view()),
+    path('notification/', SendPushNotificationToMultipleUser.as_view())
 ]
