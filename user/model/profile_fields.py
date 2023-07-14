3bb1cda23ae81f7bca7f95ada276957bc08fb667
ripon <ripon1995dc@gmail.com>
2023-07-14 17:12:15 +0600

    User model and Signal
    what?
     - model updated
     - new signal method added

--- user/model/profile_fields.py
+++ user/model/profile_fields.py
@@ -5,9 +5,7 @@ class ProfileFields(models.Model):
     image_url = models.URLField(blank=True, null=True)
     first_name = models.CharField(blank=True, null=True, max_length=30)
     last_name = models.CharField(blank=True, null=True, max_length=30)
-
-    def name(self):
-        return f"{self.first_name} {self.last_name}"
+    full_name = models.CharField(blank=True, null=True, max_length=60)
 
     class Meta:
         abstract = True
--- user/signals.py
+++ user/signals.py
@@ -1,9 +1,10 @@
-from django.contrib.auth import get_user_model
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 
+from user.models import User
 
-@receiver(pre_save, sender=get_user_model())
+
+@receiver(pre_save, sender=User)
 def update_user_image_url(sender, instance, **kwargs):
     if instance.pk:
         original_user = sender.objects.get(pk=instance.pk)
@@ -13,3 +14,14 @@ def update_user_image_url(sender, instance, **kwargs):
 
     else:
         instance.image_url = None
+
+
+@receiver(pre_save, sender=User)
+def update_profile_full_name(sender, instance, **kwargs):
+    if instance.pk:
+        original_instance = sender.objects.get(pk=instance.pk)
+        if original_instance.first_name != instance.first_name or original_instance.last_name != instance.last_name:
+            instance.full_name = f"{instance.first_name} {instance.last_name}"
+            sender.objects.filter(pk=instance.pk).update(full_name=instance.full_name)
+    else:
+        instance.full_name = None
